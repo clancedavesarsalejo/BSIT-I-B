1f87fa86e13ebe59f22990dd6bc893024d62d2a1
ArthemCombista <157579638+ArthemCombista@users.noreply.github.com>
2024-01-29 17:27:52 +0800

    Add files via upload

---
+++ Activities/Activity II/COMBISTA_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class COMBISTA_ACTIVITY_II{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Enter your first number: ");
+        int first = scanner.nextInt();
+        System.out.println("Enter your second number: ");
+        int second = scanner.nextInt();
+        
+        int sum = first + second;
+        int difference = first - second;
+        int product = first * second;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+    }
+}
