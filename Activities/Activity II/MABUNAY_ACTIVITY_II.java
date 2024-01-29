3d11a0c8049b69ccf1d52667c616567eb5abacb2
IraMariz <157578844+IraMariz@users.noreply.github.com>
2024-01-29 17:07:49 +0800

    Add files via upload

---
+++ Activities/Activity II/MABUNAY_ACTIVITY_II.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+     public class MABUNAY_ACTIVITY_II{
+       public static void main(String {}args ){
+         Scanner in = new Scanner(System.in);
+
+         System.out.print(s:"Enter your first number: ");
+         int firstNumber = scanner.nextlnt();
+         System.out.print(s:"Enter your second number: ");
+         int secondNumber = scanner.nextlnt();
+
+         
+         int sum = firstNumber + secondNumber;
+         int diff = firstNumber - secondNumber;
+         int prod = firstNumber * secondNumber;
+
+         System.out.println("Sum: " + sum);
+         System.out.println("Difference: " + difference);
+         System.out.println("Product: " + prod);
+
+
+
+
+
+       }
+    
+
+}
