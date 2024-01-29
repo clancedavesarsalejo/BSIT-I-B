8b674aa11becf1fd9f5b2c95d07b1445cbbed1b5
clancedavesarsalejo <157579179+clancedavesarsalejo@users.noreply.github.com>
2024-01-29 17:23:07 +0800

    Update Sarsalejo_Activity_II.java

--- Activities/Activity II/Sarsalejo_Activity_II.java
+++ Activities/Activity II/Sarsalejo_Activity_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Generale_Activity_II {
+public class Sarsalejo_Activity_II {
     public static void main (String []args) {
             Scanner sc = new Scanner(System.in);
             
