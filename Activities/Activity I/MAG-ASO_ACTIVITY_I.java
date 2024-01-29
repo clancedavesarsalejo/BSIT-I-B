b9cb80c2f9584d121c4a660a5609a573cfb66e54
Keith Andri T. Mag-aso <157562855+KeithAndri@users.noreply.github.com>
2024-01-29 17:22:28 +0800

    Update MAG-ASO_ACTIVITY_I.java

--- Activities/Activity I/MAG-ASO_ACTIVITY_I.java
+++ Activities/Activity I/MAG-ASO_ACTIVITY_I.java
@@ -1,8 +1,8 @@
 public class MAG-ASO_ACTIVITY_I{
     public static void main(String[] args) {
-        int num1 = 6;
-        int num2 = 18;
-        int num3 = 31;
+        int num1 = 20;
+        int num2 = 10;
+        int num3 = 5;
 
         int sum = num1 + num2 + num3;
         int diff = num1 - num2 - num3;
@@ -10,8 +10,8 @@ public class MAG-ASO_ACTIVITY_I{
         int quot = num1 / num2 / num3;
 
         System.out.println(sum);
-        System.out.println(diff);
-        System.out.println(prod);
+            System.out.println(diff);
+            System.out.println(prod);
         System.out.println(qout);
 
 
