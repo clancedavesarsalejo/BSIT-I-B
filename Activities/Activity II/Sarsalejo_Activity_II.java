9bfe1149f6481dd9c2bfc9f22868e5d116535066
clancedavesarsalejo <157579179+clancedavesarsalejo@users.noreply.github.com>
2024-01-29 17:22:47 +0800

    Update Sarsalejo_Activity_II.java

--- Activities/Activity II/Sarsalejo_Activity_II.java
+++ Activities/Activity II/Sarsalejo_Activity_II.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class Sarsalejo_Activity_II {
+public class Generale_Activity_II {
     public static void main (String []args) {
             Scanner sc = new Scanner(System.in);
             
             System.out.print("Enter your first number: ");
-            int firstNumber = scanner.nextInt();
+            int firstNumber = sc.nextInt();
             System.out.print("Enter your second number: ");
-            int secondNumber = scanner.nextInt();
+            int secondNumber = sc.nextInt();
 
             int sum = firstNumber + secondNumber;
             int difference = firstNumber - secondNumber;
