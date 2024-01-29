d8861008a22d81ccd8d60d77d6e1a787c2f3a78a
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-01-29 17:20:03 +0800

    Add files via upload

--- Activities/Activity II/CAMPO_ACTIVITY_II.java
+++ Activities/Activity II/CAMPO_ACTIVITY_II.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 public class CAMPO_ACTIVITY_II { 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner (System.in);
        
         System.out.println("Enter 1st number: ");
-		int frstnum = scanner.nextInt();
-        
-        System.out.prIntln("Enter 2nd number: ");
-		int scndnum = scanner.nextInt();
+	int frstnum = scanner.nextInt();
+        System.out.println("Enter 2nd number: ");
+	int scndnum = scanner.nextInt();
 	
         int sum = frstnum + scndnum;
         int dif = frstnum - scndnum;
         int prod = frstnum * scndnum;
     
-        System.out.print("sum is = "+ sum);
-        System.out.print("difference is = "+ dif);
-        System.out.print("product is = "+ prod);
+        System.out.print("sum is = " + sum);
+        System.out.print("difference is = " + dif);
+        System.out.print("product is = " + prod);
     
         }
        }
