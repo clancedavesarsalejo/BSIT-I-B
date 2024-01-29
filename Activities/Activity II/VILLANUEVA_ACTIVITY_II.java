5e410be4119af65c64331158642c36533b509325
Earrl <157579064+Earrl@users.noreply.github.com>
2024-01-29 17:09:40 +0800

    Add files via upload

---
+++ Activities/Activity II/VILLANUEVA_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.scanner;
+public class MAG-ASO_ACTIVITY_2 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int firstNumber = scanner,nextInt();
+        System.out.println("Enter your second number: ");
+        int SecondNumber = scanner,nextInt();
+
+        int sum = firstNumber + SecondNumber;
+        int difference = firstNumber - SecondNumber;
+        int product = firstNumber * SecondNumber;
+
+        System.out.println("Sum =" + sum);
+        System.out.println("Difference =" + difference);
+        System.out.println("Product =" + product);
+
+    
+    }
+    
+}
