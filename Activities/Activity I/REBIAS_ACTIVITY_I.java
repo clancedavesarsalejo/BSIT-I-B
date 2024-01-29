b406d93c68961f5ad2fce9d2ff1c5252ac8ef80d
rebiasjaymica <157580880+rebiasjaymica@users.noreply.github.com>
2024-01-29 17:17:58 +0800

    Add files via upload

---
+++ Activities/Activity I/REBIAS_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+public class REBIAS_ACTIVITY_I {
+
+    public static void main (String[] args) {
+
+        int x = 30;
+        int y = 40;
+        int z = 50;
+        System.out.println(x*y*z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x/y/z);
+        
+    }
+}
