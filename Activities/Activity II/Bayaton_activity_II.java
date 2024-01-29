dab304e4d1c2b6b8d2d78b87c7d2e67d46310223
BayatonFrankie <156895146+BayatonFrankie@users.noreply.github.com>
2024-01-29 17:07:49 +0800

    Add files via upload

---
+++ Activities/Activity II/Bayaton_activity_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class BAYATON_ACTIVITY_II{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print ("Enter your first number");
+        int firstNumber = scanner.nextInt();
+        System.out.print ("Enter your second number");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+
+        System.out.println("sum =" + sum);
+        System.out.println("difference =" + difference);
+        System.out.println("product ="+ product);
+
+    }
+}
+
+
+     
+        
+}   
