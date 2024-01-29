d84eb745ae8926d3a7cc29a2e1d109141fca2b95
StephanieEncabo <157580125+StephanieEncabo@users.noreply.github.com>
2024-01-29 17:27:26 +0800

    Add files via upload

---
+++ Activities/Activity II/ENCABO_ACTIVITY_II}.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ENCABO_ACTIVITY_II
+    
+    public static void main(String []args) { 
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println()"Enter your first number: ");
+        int first = scanner .nextInt();
+        System.out.println(:"Enter your second number:");
+        int second = scanner .nextInt();
+
+        int sum = first + second;
+        int difference = first - second;
+        int product = first * second;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+    }
+
+}      
+        
