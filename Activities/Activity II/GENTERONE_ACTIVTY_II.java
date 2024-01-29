2349741caf4a57457ab392a7a2c24a3e7a0da645
johnlhester <157578893+johnlhester@users.noreply.github.com>
2024-01-29 17:11:46 +0800

    Add files via upload

---
+++ Activities/Activity II/GENTERONE_ACTIVTY_II.java
@@ -0,0 +1,23 @@
+
+    import java.util.Scanner;
+public class GENTERONE_ACTIVTY_II{
+  public static void main(String []args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("1st Input Value: ");
+    int firstNumber = sc.nextInt();
+    System.out.println("2nd Input Value: ");
+    int secondNumber = sc.nextInt();
+
+    int sum = firstNumber + secondNumber;
+    int diff = firstNumber - secondNumber;
+    int product = firstNumber * secondNumber;
+
+    System.out.println("sum =" + sum);
+    System.out.println("difference =" + diff);
+    System.out.println("product =" + product);
+
+    sc.close();
+
+  }
+}
