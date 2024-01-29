a9e04d72fd8ac78b779069672b4cc0f67d11255f
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-01-29 17:26:49 +0800

    Add files via upload

---
+++ Activities/Activity II/ACAYLAR_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+  public class ACAYLAR_ACTIVITY_II {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your first number:");
+        int first = scanner.nextInt();
+        System.out.println("Enter your second number:");
+        int second = scanner.nextInt();
+
+        int sum = first + second;
+        int difference = first - second;
+        int product = first * second;
+
+        System.out.println("sum =" + sum);
+        System.out.println("difference =" + difference);
+        System.out.println("product =" + product);
+    }
+    
+}
