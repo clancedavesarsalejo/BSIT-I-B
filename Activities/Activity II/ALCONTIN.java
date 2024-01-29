dab5dcec2ba43fc6447d2ad154061ec3245b9b2a
anjenneth <157085781+eijeyy0@users.noreply.github.com>
2024-01-29 17:07:09 +0800

    Add files via upload

---
+++ Activities/Activity II/ALCONTIN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class ALCONTIN_ACTIVITY_II{
+        public static void main(String[] args);
+            Scanner In = new Scanner (System.in);
+
+            System.out.print("Enter your first number: ");
+            int firstNumber = scannerInt();
+            System.out.print("Enter your second number: ");
+            int secondNumber = scanner.nextInt();
+
+            
+            int sum = firstNumber + secondNumber;
+            int difference = firstNumber - secondNumber;
+            int product = firstNumber * secondNumber;
+
+            System.out.println("Sum = " + sum);
+            System.out.println("Difference = " + difference);
+            System.out.println("Product = " + product);
+    }
+}
+        
