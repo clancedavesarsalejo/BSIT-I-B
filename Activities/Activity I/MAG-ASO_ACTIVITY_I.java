cceae74232a51ab19454285d6527ffa01b29f7c8
Keith Andri T. Mag-aso <157562855+KeithAndri@users.noreply.github.com>
2024-01-29 17:21:20 +0800

    Add files via upload

---
+++ Activities/Activity I/MAG-ASO_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class MAG-ASO_ACTIVITY_I{
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 18;
+        int num3 = 31;
+
+        int sum = num1 + num2 + num3;
+        int diff = num1 - num2 - num3;
+        int prod = num1 * num2 * num3;
+        int quot = num1 / num2 / num3;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qout);
+
+
+
+    }
+}
