6ab55292514d844ea69e59faeb93da07b804f565
ArthemCombista <157579638+ArthemCombista@users.noreply.github.com>
2024-02-05 17:05:30 +0800

    Add files via upload

---
+++ Activities/Activity III/COMBISTA_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+    public class COMBISTA_ACTIVITY_III {
+        
+        public static void main(String[]args);{
+            Scanner sc = new Scanner (System.in);
+            System.out.print(s:"Enter you Celsius: ")
+            int temp = sc.nextInt();
+
+
+            if (temp>=100){
+
+            System.out.println(x:"Boiling");{
+            } else {
+                System.out.println(x:"Not BOILING");
+            }
+        }
+    }
