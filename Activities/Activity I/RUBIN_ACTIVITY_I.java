7da469fd82ec04a9911ab77c1d7d4ced07093d3a
Archevinculado <157578938+Archevinculado@users.noreply.github.com>
2024-02-05 17:13:48 +0800

    Add files via upload

---
+++ Activities/Activity I/RUBIN_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class RUBIN_ACTIVITY_I {
+    public static viod main(String[]args){
+        int a = 10;
+        int b = 5;
+        int c = 5;
+        
+        int sum = a + b;
+        int difference = c - a;
+        int product = a * b;
+        int quotient = c / a;
+        
+        System.err.println(sum);
+        System.err.println(difference);  
+        System.err.println(product);
+        System.err.println(quotient);
+    }
+}
