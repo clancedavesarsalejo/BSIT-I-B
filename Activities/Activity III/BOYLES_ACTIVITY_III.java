3f02aaf945ebc978a12fd297fd19bff0d7579683
Boyles, Prince Niño L <94779042+JesseMc87@users.noreply.github.com>
2024-02-05 16:56:00 +0800

    Add files via upload

---
+++ Activities/Activity III/BOYLES_ACTIVITY_III.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+    public class BOYLES_ACTIVITY_III {
+        public static void main (String [] args){
+            Scanner sc = new Scanner (System.in);
+            int temp;
+            do{
+            System.out.print("Enter Temperature (Celsius)");
+            temp = sc.nextInt();
+            if(temp>=100){
+                System.out.println("Boiling");
+             } 
+             else if(temp <= 100){
+                System.out.println("Not Boiling");
+             }
+             else{
+                System.out.println("Not Boiling");
+             }
+            }while (temp !=0);
+
+
+
+            
+
+
+        }
+    }
