e744604e659e18af7b96aab1de419c55888c9f34
tantan00t00 <157578641+tantan00t00@users.noreply.github.com>
2024-02-05 16:55:09 +0800

    Add files via upload

---
+++ Activities/Activity III/LAPINCAO_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LAPINCAO_ACTIVITY_III{
+public static void main(String[]args){
+      Scanner sc =new Scanner(System.in);
+      System.out.print("Input Temperature");
+      int temp = sc.nextInt();
+
+      if(temp>=100){
+           System.out.println("Boiling'");
+      } else {
+             System.out.println("Not Boiling");
+          }
+        }
+      }
