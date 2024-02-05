be1519ca0d9ffcb282015eb142335216e4eae187
Earrl <157579064+Earrl@users.noreply.github.com>
2024-02-05 17:15:07 +0800

    Add files via upload

---
+++ Activities/Activity III/VILLANUEVA_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class VILLANUEVA_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);   
+        System.out.println("Input a number");
+         int temp = sc.nextInt();
+      
+         if(temp>= 100){
+        System.out.println("Boiling");
+         }else {
+        System.out.println("Not Boiling");
+         }
+    }
+}   
