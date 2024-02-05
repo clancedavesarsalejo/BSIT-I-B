e6ec1fca574e2e9c354d4f567cef36b6228052b6
nadineemeterio311 <158016180+nadineemeterio311@users.noreply.github.com>
2024-02-05 16:59:17 +0800

    Add files via upload

---
+++ Activities/Activity II/public-class-EMETERIO_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class EMETERIO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First number: ");  
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter the Second number:");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+        int quot = num1 / num2;
+
+        System.out.println("Sum = "+sum);
+        System.out.println("Difference = "+diff);
+        System.out.println("Product = "+prod);
+        System.out.println("Quotient = "+quot);
+    
+
+    }
+    
+}
