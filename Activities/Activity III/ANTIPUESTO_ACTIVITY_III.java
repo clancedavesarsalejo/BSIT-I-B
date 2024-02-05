39461fef7e9ed065c9a0db7b8b1f8f47aaea95ea
RogelioAntipuestoJr <157578821+RogelioAntipuestoJr@users.noreply.github.com>
2024-02-05 16:55:28 +0800

    Add files via upload

---
+++ Activities/Activity III/ANTIPUESTO_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Antipuesto_ACTIVITY_III {
+  public static void main (String []args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Water Sensor!!");
+    System.out.println("Pls input a integer: ");
+    int x = sc.nextInt();
+    if(x >= 100) {
+      System.out.println("Boiling");
+    } else {
+      System.out.println("Not boiling");
+    }
+  }
+}
