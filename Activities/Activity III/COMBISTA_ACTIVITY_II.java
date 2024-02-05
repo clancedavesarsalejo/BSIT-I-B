05b947f03154c0efa52f87ccc7737085b52b1d72
ArthemCombista <157579638+ArthemCombista@users.noreply.github.com>
2024-02-05 17:24:46 +0800

    Update COMBISTA_ACTIVITY_II.java

--- Activities/Activity III/COMBISTA_ACTIVITY_II.java
+++ Activities/Activity III/COMBISTA_ACTIVITY_II.java
@@ -1,17 +1,14 @@
-import java.util.scanner;
-    public class COMBISTA_ACTIVITY_III {
-        
-        public static void main(String[]args);{
-            Scanner sc = new Scanner (System.in);
-            System.out.print(s:"Enter you Celsius: ")
-            int temp = sc.nextInt();
+import java.util.Scanner;
+public class COMBISTA_ACTIVITY_III{
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Please input the temperature in Celsuis: ");
+        int temp =  sc.nextInt(); 
 
-
-            if (temp>=100){
-
-            System.out.println(x:"Boiling");{
-            } else {
-                System.out.println(x:"Not BOILING");
-            }
+        if(temp >= 100){
+            System.out.println("Boiling!");
+        }else{
+            System.out.print("Not boiling");
         }
     }
+}
