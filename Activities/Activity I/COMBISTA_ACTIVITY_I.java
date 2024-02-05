f1e1faffcd846d6a5f7ce1af58a7697796ab6f45
ArthemCombista <157579638+ArthemCombista@users.noreply.github.com>
2024-02-05 15:16:16 +0800

    Add files via upload

---
+++ Activities/Activity I/COMBISTA_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class COMBISTA_ACTIVITY_I{
+    public static void main (String[]args){
+        int x = 30;
+        int y = 20;
+        int z = 50;
+        System.out.println(x*y/z);
+        System.out.println(x+y+z);
+        System.out.println(x-y-z);
+        System.out.println(x*y*z);
+        System.out.println(x/y/z);
+     }
+}
