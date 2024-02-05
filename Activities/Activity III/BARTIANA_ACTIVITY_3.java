eb5251960d6056d04bdb0c1d188fdfa2b0e7bbce
Bartiana <157724505+Bartiana@users.noreply.github.com>
2024-02-05 17:00:45 +0800

    Add files via upload

---
+++ Activities/Activity III/BARTIANA_ACTIVITY_3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.scanner;
+public class BARTIANA_ACTIVITY_3{
+
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println(" Enter the temperature in Celsius:");
+
+        int celsius = scanner.nextInt();
+
+         if   ( celsius > 100 ) {
+             System.out.println("Boiling"); }
+            else {
+              System.out.println("Not Boiling"); }
+              
+
+            }
+        }
+
+
+
+
+
+
+
+
