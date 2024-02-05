8cd0bff9a88f6dea8da76a77fb9bd8cb736df8b2
Clint Wilson N. Gonzales <157579219+clintwilson123@users.noreply.github.com>
2024-02-05 16:58:32 +0800

    Add files via upload

---
+++ Activities/Activity III/Gonzales_ACTIVITY_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Gonzales_ACTIVITY_3 {
+
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println(" Enter the temperature in Celsius:");
+
+        int celsius = scanner.nextInt();
+
+         if   ( celsius > 100 ) {
+             System.out.println("Boiling"); }
+            else {
+              System.out.println("Not Boiling"); }
+              
+
+            }
+        }
