baec39a7e8cefa28044b7c1864cef2b3c134ad3e
yvonne pono <157579481+yv0nne1@users.noreply.github.com>
2024-02-05 17:24:29 +0800

    Add files via upload

---
+++ PONO_ACTIVITY_III.java
@@ -0,0 +1,12 @@
+public class PONO_ACTIVITY_III {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Celsius":);
+        int temp = sc.nextint();
+
+        if(temp>=100){
+            System.out.println(" Boiling");
+        }else{
+            System.out.println("Not Boiling");
+        
+}
