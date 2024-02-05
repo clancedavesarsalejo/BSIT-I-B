17df571a84a2629d80505ca60b081388640f9c3f
Christiancajuelan <157580819+Christiancajuelan@users.noreply.github.com>
2024-02-05 17:06:17 +0800

    Add files via upload

---
+++ Activities/Activity III/CAJUELAN_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CAJUELAN_ACTIVITY_III {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Celsius: ");
+        int temp = sc.nextInt();
+
+        if (temp>=100){
+            System.out.println("Boiling");
+        } else {
+            System.out.println("Not Boiling");
+        }
+    }
+}
