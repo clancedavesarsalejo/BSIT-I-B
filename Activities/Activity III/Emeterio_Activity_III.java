e9394d5501481efd31aaa1bb163f41b39cb0eff0
nadineemeterio311 <158016180+nadineemeterio311@users.noreply.github.com>
2024-02-05 16:53:53 +0800

    Add files via upload

---
+++ Activities/Activity III/Emeterio_Activity_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EMETERIO_ACTIVITY_III{
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Please input the temperature in Celsuis: ");
+        int temp =  sc.nextInt(); 
+
+        if(temp >= 100){
+            System.out.println("Boiling!");
+        }else{
+            System.out.print("Not boiling");
+        }
+    }
+}
