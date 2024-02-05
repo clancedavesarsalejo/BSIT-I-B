cb8cac29b8566de615361c78b8954f4c2b6f665a
BayatonFrankie <156895146+BayatonFrankie@users.noreply.github.com>
2024-02-05 16:55:13 +0800

    Add files via upload

---
+++ Activities/Activity III/BAYATON_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class BAYATON_ACTIVITY_III {
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your celsius");
+        int temp = sc.nextInt();
+
+        if (temp>=100){
+            System.out.print("Boiling");
+        }
+        else {
+            System.out.print("Not Boiling");
+        }
+    }
+}      
+
+
+    
+
