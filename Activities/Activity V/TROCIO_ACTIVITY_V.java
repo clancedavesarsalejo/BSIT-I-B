7ccb54b734fdab96bf6893546d99099a3e0c1e0d
Trociolove <159006195+Trociolove@users.noreply.github.com>
2024-02-12 16:59:53 +0800

    Add files via upload

--- Activities/Activity V/TROCIO_ACTIVITY_V.java
+++ Activities/Activity V/TROCIO_ACTIVITY_V.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 public class TROCIO_ACTIVITY_V {
     public static void main(String[] args) {
-        Scanner sr = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         int age;
-        System.out.println("Enter your age");
-        age = sr.nextInt();
-        if (age>0 && age <=11){
-            System.out.println("You are a child");
-    }else if (age>=12 && age <= 17){
-        System.out.println("You are a teenager");
-    }else if (age>=18 && age <=64){
-        System.out.println("Go home adult man");
-}
+
+        System.out.print("Enter age: ");
+        age = input.nextInt();
+        if(age>= 0 && age <=11){
+            System.out.print("Child");
+
+        }else if(age >=12 && age <= 17){
+            System.out.println("Teen");
+
+        }else if(age >=18 && age <=64){
+            System.out.println("Adult");
+        }
     }
 }
+
+    
+        
+    
