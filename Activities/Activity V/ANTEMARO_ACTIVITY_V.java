c7f4760e687565b2b7df46cebc61df3dce643908
BlessAntemaro <157581023+BlessAntemaro@users.noreply.github.com>
2024-02-12 16:59:15 +0800

    Add files via upload

---
+++ Activities/Activity V/ANTEMARO_ACTIVITY_V.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ANTEMARO_ACTIVITY_V {
+    public static void main (String []args){
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.print("ENTER AGE");
+        int age = sc.nextInt();
+
+        if(age<=64 && age >=18){
+            System.out.println("ADULT");
+        }else if (age<=17 && age >=12){
+            System.out.println("TEEN");
+        }else{
+            System.out.println("CHILD");
+        }
+    }
+}
+
+ 
