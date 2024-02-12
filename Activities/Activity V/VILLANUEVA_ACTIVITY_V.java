ad5e110540fc2fc0c8ab2bccc8d2786403f127b6
Earrl <157579064+Earrl@users.noreply.github.com>
2024-02-12 16:59:55 +0800

    Add files via upload

---
+++ Activities/Activity V/VILLANUEVA_ACTIVITY_V.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class VILLANUEVA_ACTIVITY_V {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int age;
+        
+        System.out.print("Enter age: ");
+        age = input.nextInt();
+        if(age>= 0 && age <= 11){
+            System.out.print("Child");
+
+        }else if(age >=12 && age <= 17) {
+            System.out.println("Teen");
+
+        }else if(age >=18 && age <=64) {
+            System.out.println("Adult");
+        }
+    }
+}
