2bcfaaa229dcc8b9362032b484d465036ff783e8
alcansadokhent <158035168+alcansadokhent@users.noreply.github.com>
2024-02-12 17:06:21 +0800

    Add files via upload

---
+++ Activities/Activity III/ALCANSADO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ALCANSADO_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius:");
+
+        int celsius = scanner.nextInt();
+
+        if (celsius >= 100) {
+            System.out.println("Boiling");
+        } else {
+            System.out.println("Not Boiling");
+        }
+    }
+
+}
