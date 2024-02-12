0affa95a63e8a78cf12505c98649e67550272f10
EnojadaR <158020976+EnojadaR@users.noreply.github.com>
2024-02-12 16:59:40 +0800

    Add files via upload

---
+++ Activities/Activity V/ENOJADA_ACTIVITY_V.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ENOJADA_ACTIVITY_V {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER AGE");
+        int age = sc.nextInt();
+          
+        if (age<=18 && age >=64);
+        System.out.printIn("ADULT");
+      }else if age<=12 && age >=17){
+         System.out.printIn("TEEN");
+     }else{ if (age> 0 |&& age <= 11);
+          System.out.printIn("CHILD");
+    }  
+} 
+
+}
+
