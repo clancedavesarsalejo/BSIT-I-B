cc3e2740bf30e3baab989f00748f94757f49dc4e
Ajparian <157578718+Ajparian@users.noreply.github.com>
2024-02-12 01:03:46 -0800

    Add files via upload

---
+++ Activities/Activity III/JULITO_ACTIVITY.III.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PARIAN_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print( "Enter Celcius: ");
+        int temp = scanner.nextInt();
+
+
+        if (temp < 100) {
+            System.out.println("Not Boiling");
+         } else {
+            System.out.println("Boiling");
+         }
+        }
+    }
+    
+}
