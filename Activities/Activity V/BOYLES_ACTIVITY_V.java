29257cfd6b00b20fe563fa0fd9eea9fd3c240de2
Boyles, Prince Niño L <94779042+JesseMc87@users.noreply.github.com>
2024-02-12 17:00:14 +0800

    Add files via upload

---
+++ Activities/Activity V/BOYLES_ACTIVITY_V.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class BOYLES_ACTIVITY_V{
+        public static void main(String []args){
+            Scanner Age = new Scanner(System.in);
+         
+            System.out.print("Enter Age: "); 
+            int age = Age.nextInt();
+
+            if (age >= 0 && age <= 11){
+                System.out.println("child");
+            } else if (age >= 12 && age <= 17) 
+            {
+                System.out.println("Teen");
+            } else if (age >= 18 && age >= 64) 
+            {
+                System.out.println("Adult");
+            }
+           
+        }
+
+    }
