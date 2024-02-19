660d0bb3b1ed17d49c9bfc91c99aac95d0d4401a
Trociolove <159006195+Trociolove@users.noreply.github.com>
2024-02-19 17:47:14 +0800

    Add files via upload

--- Activities/Activity VI/TROCIO_ACTIVITY_VI.java
+++ Activities/Activity VI/TROCIO_ACTIVITY_VI.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
-public class TROCIO_ACTIVITY_VI {
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of time seconds: ");
-        int time;
-        time = sc.nextInt();
-        while(time >=0){
-        System.out.println(time);
-        time--;
+ import java.util.Scanner;
+     public class TROCIO_ACTIVITY_VI {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your timer: ");
+        int time = input.nextInt();
+
+        while (time>=0){
+            System.out.println(time);
+            time--;
+            
         }
+     }
     }
-}
+    
