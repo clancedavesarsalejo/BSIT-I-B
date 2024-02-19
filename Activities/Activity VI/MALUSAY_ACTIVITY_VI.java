dced09850124a927ffaeb8faca153cbdae88be2e
juliefel <157578767+juliefel12@users.noreply.github.com>
2024-02-19 17:47:22 +0800

    Add files via upload

---
+++ Activities/Activity VI/MALUSAY_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MALUSAY_ACTIVITY_VI {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter time: ");
+        int time = sc.nextInt();
+
+        while(time >= 0 ){
+            System.out.println(time);
+            time --;
+        }
+     }
+   }
+
+
+
