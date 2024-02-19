a9d635dd2f749463ddca3a52caeac8b225eb7b13
RogelioAntipuestoJr <157578821+RogelioAntipuestoJr@users.noreply.github.com>
2024-02-19 17:45:15 +0800

    Add files via upload

---
+++ Activities/Activity VI/ANTIPUESTO_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ANTIPUESTO_ACTIVITY_VI {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number Of Time");
+        int num = sc.nextInt();
+
+        while(num>=0) {
+            System.out.println(num);
+
+            num--;
+        }
+    }
+}
