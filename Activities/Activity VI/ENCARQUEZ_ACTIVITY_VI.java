5736beb8aa9f13ad117f13af031321dc74b12b20
GlyzaEncarquez <157580265+GlyzaEncarquez@users.noreply.github.com>
2024-02-19 17:45:52 +0800

    Add files via upload

---
+++ Activities/Activity VI/ENCARQUEZ_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+   public class ENCARQUEZ_ACTIVITY_VI{
+
+
+     public static void main(String[]args){
+         Scanner sc = new Scanner (System.in);
+         int num;
+         System.out.println("Enter number: ");
+         int count = sc.nextInt();
+
+         while (count >=0){
+
+          System.out.println(count);
+          count --;
+
+         }   
+
+     }
+
+
+   
