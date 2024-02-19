9e5fde400181824b6e1eaa606645c072301369b9
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-02-19 17:47:02 +0800

    Update ACAYLAR_ACTIVITY_VI.java

--- Activities/Activity VI/ACAYLAR_ACTIVITY_VI.java
+++ Activities/Activity VI/ACAYLAR_ACTIVITY_VI.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ACAYLAR_ACTIVITy_VII {
+public class ACAYLAR_ACTIVITy_VI {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
