4ce1674d951e6e4f06ee2f8b79cb1a70b47528f2
johnlhester <157578893+johnlhester@users.noreply.github.com>
2024-02-19 17:45:21 +0800

    Add files via upload

---
+++ Activities/Activity VI/GENTERONE_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner ;
+public class GENTERONE_ACTIVITY_VI{
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+
+
+    System.out.println("Input the number of seconds: ");
+  
+    int num =sc.nextInt();
+    while(num>=0) {
+    System.out.println(num);
+    num--;
+    }
+
+    }
+
+
+	}
+
+	
