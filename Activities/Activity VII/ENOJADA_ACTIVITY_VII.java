473abacc5e3321be6aa236e5c2823b15149c4f1f
EnojadaR <158020976+EnojadaR@users.noreply.github.com>
2024-02-19 17:46:40 +0800

    Add files via upload

---
+++ Activities/Activity VII/ENOJADA_ACTIVITY_VII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public  class ENOJADA_ACTIVITY_VII {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Factorial Number;");
+        long number = sc.nextInt();
+        long fact = 1;
+        for(long x=1;x<=number;x++){
+            fact*=x;
+        }
+        System.out.print(fact);
+        sc.close();
+        
+    }
+
+
+}
