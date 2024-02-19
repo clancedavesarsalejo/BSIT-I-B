9e8c9f042c00f53654e36b14293098bddb2f670e
tantan00t00 <157578641+tantan00t00@users.noreply.github.com>
2024-02-19 17:45:20 +0800

    Add files via upload

---
+++ Activities/Activity VI/LAPINCAO_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+    
+import java.util.Scanner;
+public class LAPINCAO_ACTIVITY_VI {
+public static void main (String []args){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Input a Number");
+int x  = sc.nextInt();
+
+do{
+System.out.println(x);
+x--;
+}while(x >=0);
+
+
+
+
+
+}    
+}
