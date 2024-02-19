e22d214328b127bfcd36be6ef0de6461bcc7ff37
QUISIDO, KEN <157578930+kendidnt@users.noreply.github.com>
2024-02-19 17:45:09 +0800

    Add files via upload

---
+++ Activities/Activity VI/QUISIDO_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class QUISIDO_ACTIVITY_VI {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter your timer: ");
+            int time = input.nextInt();
+
+            while(time>=0){
+                System.out.println(time);
+                time--;
+            }
+
+        }
+        
+    }
