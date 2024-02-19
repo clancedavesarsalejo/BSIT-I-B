314e624575607765691375b6020039c9bddb28db
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-02-19 17:45:12 +0800

    Add files via upload

---
+++ Activities/Activity VI/CAMPO_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner;
+public class CAMPO_ACTIVITY_VI {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("enter seconds: ");
+	int x = sc.nextInt();
+
+	while(x >= 0){
+	System.out.println(x);
+	x--;
+
+}}}
+
+
