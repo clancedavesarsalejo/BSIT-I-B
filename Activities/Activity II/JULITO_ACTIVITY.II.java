9ac41ba2141fd39515b885cdd09c145748c6b3cd
Ajparian <157578718+Ajparian@users.noreply.github.com>
2024-02-19 17:47:37 +0800

    PARIAN_ACTIVITY.II.java

--- Activities/Activity II/JULITO_ACTIVITY.II.java
+++ Activities/Activity II/JULITO_ACTIVITY.II.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-public class PARIAN_ACTIVITY_III {
-    public static void main(String[] args) {
+  public class PARIAN_ACTIVITY_II {
+    public static void main(String[]args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print( "Enter Celcius: ");
-        int temp = scanner.nextInt();
+        System.out.println("Enter your first number:");
+        int first = scanner.nextInt();
+        System.out.println("Enter your second number:");
+        int second = scanner.nextInt();
 
+        int sum = first + second;
+        int difference = first - second;
+        int product = first * second;
 
-        if (temp < 100) {
-            System.out.println("Not Boiling");
-         } else {
-            System.out.println("Boiling");
-         }
-        }
+        System.out.println("sum =" + sum);
+        System.out.println("difference =" + difference);
+        System.out.println("product =" + product);
     }
     
 }
