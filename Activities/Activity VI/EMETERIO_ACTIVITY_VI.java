5a2c5ab5b3acf14de1a9e4c90325514c12a67b06
nadineemeterio311 <158016180+nadineemeterio311@users.noreply.github.com>
2024-02-19 17:45:17 +0800

    Add files via upload

---
+++ Activities/Activity VI/EMETERIO_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EMETERIO_ACTIVITY_VI{
+    public static void main (String []args){
+        Scanner sc= new Scanner(System.in);
+        
+
+        System.out.print("Enter Countdown " );
+        int count= sc.nextInt();
+
+    while(count>=0  ){
+System.out.println(count);
+count --;
+    }
+        
+        
+    
+
+        
+    }
+}
