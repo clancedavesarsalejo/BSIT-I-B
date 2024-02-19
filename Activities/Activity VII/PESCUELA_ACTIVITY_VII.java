eb972002dfc62b3785f5bc670c378a245a7e941e
RayLeonardPescuela <157578744+RayLeonardPescuela@users.noreply.github.com>
2024-02-19 17:45:54 +0800

    Add files via upload

---
+++ Activities/Activity VII/PESCUELA_ACTIVITY_VII.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PESCUELA_ACTIVITY_VII {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your Number: ");
+    long number = sc.nextInt();
+    long fact = 1;
+    for(long x=1; x <= number;x++){
+        fact*=x;
+    }
+    System.out.print("Factorial of "+number+" is "+fact);
+
+    
+}
+}
