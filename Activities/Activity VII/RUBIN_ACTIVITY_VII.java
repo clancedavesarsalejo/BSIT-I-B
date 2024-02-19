1f791abdb5980f2471af32cc28b4688322ebc9e7
Archevinculado <157578938+Archevinculado@users.noreply.github.com>
2024-02-19 17:49:02 +0800

    Add files via upload

--- Activities/Activity VII/RUBIN_ACTIVITY_VII.java
+++ Activities/Activity VII/RUBIN_ACTIVITY_VII.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
-public class RUBIN_ACTIVITY_VI {
+public class RUBIN_ACTIVITY_VII {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter the number of seconds: ");
-        int num = sc.nextInt();
-        
-        while(num>= 1){
-            System.out.println(num); 
-            num--; 
+        System.out.print("Enter the number: ");
+        long number = sc.nextInt();
+        long fact = 1;
+        for(long x=1; x<= number;x++){
+            fact*=x;
         }
-        
-        System.out.println("countdown fineshed");
+        System.out.print("The Answer is: " + fact);
         sc.close();
-
-    } 
-    
+    }
 }
