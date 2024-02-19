35cf51cf11b5e792351f16ac9db0ca6a555d9312
Joseph M. Generale <156919590+josephgenerale@users.noreply.github.com>
2024-02-19 17:46:08 +0800

    Add files via upload

---
+++ Activities/Activity VI/Generale_Activity_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Generale_Activity_VI {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your number in seconds: ");
+        int num = sc.nextInt();
+
+        while(num>= 0){
+        System.out.println(num);
+        num--;
+        }  
+        System.out.println("Countdown Finished, You're ready to go.");
+        sc.close();  
+    }
+    
+}
