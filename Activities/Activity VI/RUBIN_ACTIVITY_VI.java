2357554ddffbd06e9f9e148c6e9631846ab3af81
Archevinculado <157578938+Archevinculado@users.noreply.github.com>
2024-02-19 17:49:56 +0800

    Add files via upload

--- Activities/Activity VI/RUBIN_ACTIVITY_VI.java
+++ Activities/Activity VI/RUBIN_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class RUBIN_ACTIVITY_VII {
+public class RUBIN_ACTIVITY_VI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
