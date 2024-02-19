f2cc089c74b27976da536398b3529dbfc65dda09
francisvera <157578852+francisvera@users.noreply.github.com>
2024-02-19 17:45:55 +0800

    Add files via upload

---
+++ Activities/Activity VI/VERA_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class VERA_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+
+        System.out.println("enter the number of seconds:");
+        int num =sc.nextInt();
+        while(num>=0) { 
+        System.out.println(num); 
+         num--;
+
+        }
+
+   }
+}
