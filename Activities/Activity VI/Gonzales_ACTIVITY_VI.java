675961f9ce298220226f21516d710c91e94e7d8e
Clint Wilson N. Gonzales <157579219+clintwilson123@users.noreply.github.com>
2024-02-19 17:47:04 +0800

    Add files via upload

---
+++ Activities/Activity VI/Gonzales_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Gonzales_ACTIVITY_VI {
+
+    public static void main (String[]args) {
+        Scanner sc = new Scanner (System.in);
+        int sec = 5;
+        System.out.print("Enter the Countdown:");
+        int time = sc.nextInt();
+
+
+
+        while(sec>=0) {
+            System.out.println(sec);
+            sec--;
+        }
+
+
+
+
+    }
+}
