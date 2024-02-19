cef0b6362fc1ec28e0fc171474aecfa2dd9a0c20
anjenneth <157085781+eijeyy0@users.noreply.github.com>
2024-02-19 17:46:08 +0800

    Add files via upload

---
+++ Activities/Activity VI/ALCONTIN_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ALCONTIN_ACTIVITY_VI {
+public static void main (String []args){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Input a Number");
+int x = sc.nextInt();
+
+do{
+System.out.println(x);
+x--;
+}while(x>=0);
+
+
+
+
+
+}    
+}
