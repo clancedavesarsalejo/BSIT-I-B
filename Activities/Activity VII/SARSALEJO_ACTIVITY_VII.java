f2058b4b6d56cdfc622b54e6aade473e20aa6118
clancedavesarsalejo <157579179+clancedavesarsalejo@users.noreply.github.com>
2024-02-19 17:46:14 +0800

    Add files via upload

---
+++ Activities/Activity VII/SARSALEJO_ACTIVITY_VII.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SARSALEJO_ACTIVITY_VII {
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        long number = sc.nextInt();
+        long fact = 1;
+        for(long x=1; x<= number;x++){
+            fact*=x;
+        }
+        System.out.print(fact);
+        sc.close();
+    }
+}                
