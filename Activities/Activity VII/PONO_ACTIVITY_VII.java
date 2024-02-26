6cbe79b9f91816564cf5a24a352c19e27e719d21
yvonne pono <157579481+yv0nne1@users.noreply.github.com>
2024-02-26 14:25:03 +0800

    Add files via upload

---
+++ Activities/Activity VII/PONO_ACTIVITY_VII.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PONO_ACTIVITY_VII {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        long factorial = 1;
+
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+
+        for(int i =1; i<=num; i++){
+            factorial*=i;
+        }
+        System.out.println("The factorial of" + num+" is ");
+        factorial);
+        
+    }
+}
