3dbb73b4a59453f1d2947bfbfdc8e2bd62d3643d
IraMariz <157578844+IraMariz@users.noreply.github.com>
2024-02-26 14:30:29 +0800

    Add files via upload

---
+++ Activities/Activity I/MABUNAY_ACTIVITY1.java
@@ -0,0 +1,18 @@
+public class MABUNAY_ACTIVITY_I { 
+    public static void main(String[] args) {
+   
+   int A =2;
+   int B =30;
+   int C =20;
+
+   int SUM= A+C; 
+   int DIFFERENCE= B-A; 
+   int PRODUCT= B*A; 
+   int QUOTIENT= A/C; 
+   
+   System.out.println("Addition:"+SUM);
+   System.out.println("Subtraction:"+DIFFERENCE);
+   System.out.println("Multiplication:"+PRODUCT);
+   System.out.println("Division:"+QUOTIENT);
+    }
+   }
