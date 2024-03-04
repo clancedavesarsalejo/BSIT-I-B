a9f713d9b618dc6b6c1d66158cdc1ff31f9bdf17
Bartiana <157724505+Bartiana@users.noreply.github.com>
2024-03-04 17:36:05 +0800

    Add files via upload

---
+++ Activities/Activity I/BARTIANA_ACTIVITY_I.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class BARTIANA_ACTIVITY_I {
+
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter the name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.print("Enter the number of the name you would like to select (1-5): ");
+        int selection = sc.nextInt();
+        
+        
+        if (selection >= 1 && selection <= 5) {
+            
+            String selectedName = names[selection - 1];
+            System.out.println("You have selected: " + selection + " is: " + selectedName);
+        } else {
+            System.out.println("Invalid selection.");
+        }
+
+        sc.close(); 
+    }
+}
