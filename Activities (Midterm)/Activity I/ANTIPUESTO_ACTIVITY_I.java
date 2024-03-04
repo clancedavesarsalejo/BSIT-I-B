11e5a253d2d4e61df00b4e919d7d0108bde98608
RogelioAntipuestoJr <157578821+RogelioAntipuestoJr@users.noreply.github.com>
2024-03-04 17:37:08 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/ANTIPUESTO_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+import  java.util.Scanner;
+public class ANTIPUESTO_ACTIVITY_I {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	String[]names =new String[5];
+
+	for (int i=0; i<names.length;i++ ){
+	System.out.println("enter name"+(i+1)+" :");
+	names[i]=sc.nextLine();}
+
+System.out.println("enter number of name to select (1-5):");
+int name=sc.nextInt();
+
+if (name>=1&&name<=names.length){
+
+System.out.println("you selected: "+ names[name-1]+".");
+}
+else{
+System.out.println("invalid");
+}
+}
+}
