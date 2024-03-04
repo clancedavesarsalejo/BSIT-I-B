d36024e3f015ede3a0f320f2e903f7f1789d9531
Trociolove <159006195+Trociolove@users.noreply.github.com>
2024-03-04 17:38:57 +0800

    Add files via upload

--- Activities (Midterm)/Activity I/TROCIO_ACTIVITY_I.java
+++ Activities (Midterm)/Activity I/TROCIO_ACTIVITY_I.java
@@ -1,23 +1,23 @@
-import java.util.Scanner;
-public class TROCIO_ACTIVITY_I {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] names = new String[5];
-
-        
-        for (int i = 0 ; i < names.length ; i++ ){
-
-            System.out.print("Enter a name: "+(i+1)+" : ");
-        names[i] = sc.nextLine();
-            
-        }
-        System.out.print("Please input the number of the list of names you would like to display: ");
-        int n = sc.nextInt();
-        if(n>= 1 && n <= names.length){
-         System.out.println(names[n-1]);
-        }else{
-            System.out.println("Invalid ");
-        }
-
-        }
 }
+import java.util.Scanner;
+   public class TROCIO_ACTIVITY_I{
+   public static void main (String[]args){
+    Scanner sc = new Scanner(System.in);
+
+    String [] names = new String[5];
+
+     for (int i = 0; i < names.length; i++){
+       System.out.print("Enter Name"+(i + 1) +": ");
+       names [i] = sc.nextLine();
+     }
+
+     System.out.print("Enter the number on the list of names you want to display: ");
+     int index = sc.nextInt();
+
+     if (index >= 1 && index <= names.length){
+     System.out.print("You have selected "+ names[index - 1]+ ",");
+     }else {
+        System.out.print("Invalid Index");
+         }
+    }
+ }
