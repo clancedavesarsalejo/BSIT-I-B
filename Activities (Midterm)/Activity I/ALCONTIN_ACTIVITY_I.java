1ea161cbb4b69c4dd955ee26f6eec08fb1aaed0d
anjenneth <157085781+eijeyy0@users.noreply.github.com>
2024-03-04 17:36:48 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/ALCONTIN_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+    public class ALCONTIN_ACTIVITY_I{
+    public static void main(String []args){
+        Scanner S = new Scanner(System.in);
+
+        String [] names = new String[5];
+
+    for(int i=0;i< names.length; i++){
+        System.out.print("Enter Name"+(i+1)+": ");
+        names[i] = S.nextLine();
+    }
+
+    System.out.print("Enter number of list of names: ");
+    int index = S.nextInt();
+
+    if (index >= 1 && index <= names.length) {
+        System.out.println("You have Selected: " + names[index - 1]+ ".");
+    }else {
+        System.out.println("Invalid index");
+    
+    }
+    }
+
+}
