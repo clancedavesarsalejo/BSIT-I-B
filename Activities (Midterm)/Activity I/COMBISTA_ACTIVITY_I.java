df1f498e3b49a9611ed189e2cb5eca4bb748000a
ArthemCombista <157579638+ArthemCombista@users.noreply.github.com>
2024-03-04 17:40:03 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/COMBISTA_ACTIVITY_I.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class COMBISTA_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[5];
+
+        
+        for (int i = 0 ; i < names.length ; i++ ){
+
+            System.out.print("Enter a name: "+(i+1)+" : ");
+        names[i] = sc.nextLine();
+            
+        }
+        System.out.print("Please input the number of the list of names you would like to display: ");
+        int n = sc.nextInt();
+        if(n>= 1 && n <= names.length){
+         System.out.println(names[n-1]);
+        }else{
+            System.out.println("Invalid ");
+        }
+
+        }
+}
+
+
+
