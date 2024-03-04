be8614e7ef77850329176c8e452d94a834b972a7
tommysumonod <157578822+tommysumonod@users.noreply.github.com>
2024-03-04 17:35:52 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/SUMONOD_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SUMONOD_ACTIVITY_I {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    String[] name = new String[5];
+
+    for(int i=0;i<5;i++){
+      System.out.print("Enter Complete names " + (i + 1) + ": ");
+      name[i] = sc.nextLine();
+    }
+   System.out.print("Enter a number: "); 
+   int num = sc.nextInt();
+   
+   if(num >= 1 && num <= name.length){
+    System.out.println(name[num - 1]);
+   }else{
+    System.out.println("Invalid");
+   }
+    
+    sc.close();
+  }
+}
