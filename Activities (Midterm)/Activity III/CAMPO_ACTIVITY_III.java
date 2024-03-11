679ef16c7b6bfebf0968db6db4e49c9d8c0d7f82
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-03-11 18:45:45 +0800

    Renamed CAMPO_ACTIVITY-III to CAMPO_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/CAMPO_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/CAMPO_ACTIVITY_III.java
@@ -1,37 +1,25 @@
-import  java.util.Scanner;
-public class CAMPO_ACTIVITY_III {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
+public class CAMPO_ACTIVITY_III{
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
 
-		int[][] Score = {
-		            {1, 2, 3, 4, 5},
-		            {6, 7, 8, 9, 10},
-		            {11, 12, 13, 14, 15},
-		            {16, 17, 18, 19, 20},
-		            {21, 22, 23, 24, 25}
-		        };
+		int rows = 5;
+		int columns = 5;
+		int[][] scores = new int[rows][columns];
 
-		        int sum = 0;
+		for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+		System.out.print("Enter score at [" + i + "][" + j + "]: ");
+		scores[i][j] = sc.nextInt();
+	}}
 
-		        for (int i = 0; i < 25; i++) {
-		            System.out.print("Input Score" + ": ");
-		            int score = sc.nextInt();
+		int sum = 0;
+		for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+		sum += scores[i][j];
+	}}
 
-		            sum += score;
+		double average = (double) sum / (rows * columns);
+		System.out.println("Average = " + average);
 
-
-		            for (int x = 0; x < Score.length; x++) {
-		                for (int y = 0; y < Score[x].length; y++) {
-		                    if (Score[x][y] == score) {
-		                        System.out.println("Score at position (" + x + "," + y + ") is " + score);
-		                    }
-		                }
-		            }
-		        }
-
-		        System.out.println("Sum of all scores: " + sum);
-
-		        double average = (double) sum / 25;
-		        System.out.println("Average of all scores is: " + average);
-		    }
-}
+}}
