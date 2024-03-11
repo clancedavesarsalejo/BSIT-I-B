ed1e2d9e092ef551235450ca0a2eda47fa4e1979
tantan00t00 <157578641+tantan00t00@users.noreply.github.com>
2024-03-11 17:25:08 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/LAPINCAO_ACTIVITY_III.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class LAPINCAO_ACTIVITY_III {
+ public static void main (String []args){
+    Scanner sr = new Scanner (System.in);
+
+    final int ROWS = 5;
+    final int COLS = 5;
+
+    int[][] scores = new int[ROWS][COLS];
+    Scanner scanner = new Scanner(System.in);
+
+   
+    System.out.println("Enter scores for 5 students:");
+    for (int i = 0; i < ROWS; i++) {
+        System.out.println("Enter scores for student " + (i + 1) + ": ");
+        for (int j = 0; j < COLS; j++) {
+            scores[i][j] = scanner.nextInt();
+        }
+    }
+
+    
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            sum += scores[i][j];
+            count++;
+        }
+    }
+    double average = (double) sum / count;
+
+    
+    System.out.println("Average of all scores: " + average);
+
+    
+    scanner.close();
+}
+}
