69849b03235dfc11eb008a132add361353b1ecdc
yvonne pono <157579481+yv0nne1@users.noreply.github.com>
2024-03-11 17:25:31 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/PONO_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PONO_ACTIVITY_III{
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+        int [][] scores = new int[5][5];
+      for(int x=0; x<scores.length ; x++){
+        for(int y=0; y<scores.length ; y++){
+            System.out.println("Enter the Column "+(x+1)+" of row"+(y+1)+" : ");
+       scores[x][y] = sc.nextInt();
+        }
+        
+    }
+ }
+}
