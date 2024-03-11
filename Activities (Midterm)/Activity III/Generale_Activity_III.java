2bc2b230a2b0d047141a1de84b52a45a5a2a1f68
Joseph M. Generale <156919590+josephgenerale@users.noreply.github.com>
2024-03-11 17:25:01 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/Generale_Activity_III.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Generale_Activity_III {
+    public static void main (String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        int [][] score = new int [5] [5];
+        for (int x=0; x<score.length; x--){
+            for(int y=0; y<score.length; y++){
+            System.out.print("Enter your score" + " of row #" + (y+1)+ ": ");
+            score [x] [y] = sc.nextInt();
+            }
+            int sum=0;
+            System.out.println(sum % score.length);
+        }
+        
+        sc.close();
+    }
+}
