4f51969b313e67ad544f50ed4b2e3705188ae422
clancedavesarsalejo <157579179+clancedavesarsalejo@users.noreply.github.com>
2024-03-11 17:28:10 +0800

    Update Sarsalejo_Activity_III.java

--- Activities (Midterm)/Activity III/Sarsalejo_Activity_III.java
+++ Activities (Midterm)/Activity III/Sarsalejo_Activity_III.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class Sarsalejo_Activity_III{
-    public static void main(String[]args){     
+    public static void main (String []args){
+        
         Scanner sc = new Scanner(System.in);
-        int [][] scores = new int[5][5];
-        for(int x = 0; x < scores.length; x++){
-            for(int y = 0; y < scores.length; y++){
-                System.out.print("Enter Your Column "+(x+1)+ " of row " + (y=1) + ": ");
-                scores[x][y] = sc.nextInt(); 
+        int [][] score = new int [5] [5];
+        for (int x=0; x<score.length; x--){
+            for(int y=0; y<score.length; y++){
+            System.out.print("Enter your score" + " of row #" + (y+1)+ ": ");
+            score [x] [y] = sc.nextInt();
             }
-            
-            
-        }       
+            int sum=0;
+            System.out.println(sum % score.length);
+        }
+        
+        sc.close();
     }
- }
+}
