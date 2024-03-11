2775598e4ad3feb7022a6387afe696fd8d4574da
tommysumonod <157578822+tommysumonod@users.noreply.github.com>
2024-03-11 17:25:54 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/SUMONOD_ACTIVITY_III.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SUMONOD_ACTIVITY_III {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+
+    int[][] average = new int[5][5];
+
+    for(int x = 0;x < 5; x++){
+      for(int y = 0;y < 5; y++){
+        System.out.print("Enter Value " + (x + 1) + ": ");
+        average[x][y] = sc.nextInt();
+      }
+    }
+    double sum = 0;
+    for(int i = 0;i < average.length; i++){
+      for(int o = 0;o < average[i].length; o++){
+        sum += average[i][o];
+      }
+    }
+    System.out.println(sum / average.length);
+
+    sc.close();
+  }
+}
