e7483d2433ef6afde6fa6292b7932927b93fbc8e
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-04-15 17:23:23 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/CAMPO_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Campo_ACTIVITY_II {
+
+    static void feetconvert(double feet) {
+	double inches = feet * 12;
+	System.out.print("inches = " +inches);
+}
+	public static void main(String[]args){
+	Scanner sc= new Scanner(System.in);
+	System.out.print("Enter the feet: ");
+	double feet = sc.nextDouble();
+	feetconvert(feet);
+}}
