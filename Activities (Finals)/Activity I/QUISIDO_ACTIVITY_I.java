0fc85c52370309b8a8fe2992992a0b5d82073261
QUISIDO, KEN <157578930+kendidnt@users.noreply.github.com>
2024-04-15 17:30:08 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/QUISIDO_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class QUISIDO_ACTIVITY_I {
+    public static void main(String args[]) 
+    {   
+
+        int length = getlength();
+        int width = getWidth() ;
+        int area = length*width;
+         System.out.println("Area - " + area);    
+   }
+   static int getlength(){
+      Scanner input = new Scanner (System.in);
+      System.out.println("Input your Length");
+      return input.nextInt();
+   }
+   static int getWidth(){
+      Scanner input = new Scanner (System.in);
+      System.out.println("Input your Width");
+      return input.nextInt();
+}
+}
