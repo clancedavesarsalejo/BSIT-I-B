d4693e3fce3f875ce0c849b61c436cf25d5b8dad
EnojadaR <158020976+EnojadaR@users.noreply.github.com>
2024-04-15 16:38:13 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/ENOJADA_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ENOJADA_ACTIVITY_I {
+
+
+static int length(){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the lenght of the Rectangle ");
+    return sc.nextInt();
+}
+static int width(){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the width of the Rectangle ");
+return sc.nextInt();
+}
+
+
+public static void main(String[]args) {
+long area = width()* length();
+System.out.println("Area of the rectangle:" + result);
+}
+}
