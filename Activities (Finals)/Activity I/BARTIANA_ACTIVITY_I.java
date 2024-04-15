fead48934744798857d29ff02c499eba815de16b
Bartiana <157724505+Bartiana@users.noreply.github.com>
2024-04-15 17:24:19 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/BARTIANA_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BARTIANA_ACTIVITY_I{
+
+    public static void main(String[] args) {
+        
+    }
+     static void main(String[] args) {
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")        
+
+    }
+
+
+}
