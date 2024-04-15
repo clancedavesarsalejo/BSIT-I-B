55f5f10f9b5b914f07c01349bfaf277fa7bcfe3e
BayatonFrankie <156895146+BayatonFrankie@users.noreply.github.com>
2024-04-15 16:37:28 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/BAYATON_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class BAYATON_ACTIVITY_I{
+     
+    String int length(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        return sc. nextline();
+    }
+
+    static int width(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the width of the rectangle: ");
+        return sc. nextline();
+    }
+
+    public static void main(String[]args){
+        int result = width()* length();
+        System.out.print("Area of the rectangle: "+ result);
+    }
+}
+        
+
+    
