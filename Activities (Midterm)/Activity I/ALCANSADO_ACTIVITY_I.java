ec9437a5c67d4a967e58af0fa3b38ee3877b1f75
alcansadokhent <158035168+alcansadokhent@users.noreply.github.com>
2024-04-15 17:32:27 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity I/ALCANSADO_ACTIVITY_I.java
@@ -0,0 +1,25 @@
+public import java.util.Scanner;
+
+public class ALCANSADO_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Enter name" + (1 + 1) + ":");
+        }
+
+        System.out.print("Enter the number on the list of names you want to display: ");
+        int index = sc.nextInt();
+
+        if (index >= 1 && index <= names.length) {
+            System.out.println("You have selected " + names[index - 1] + ".");
+        } else {
+            System.out.println("invalid index");
+        }
+    }
+} {
+    
+}
