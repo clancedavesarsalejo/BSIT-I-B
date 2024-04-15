101651eba1df2c27cbcc1df7baf6646f10700b62
C. Adrian Jay <117725727+K1gwa@users.noreply.github.com>
2024-04-15 17:22:48 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/CAMPO_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CAMPO_ACTIVITY_one{
+public static void main(String[]args){
+int length = lengthinpt();
+int width = widthinpt();
+int area = length * width;
+System.out.println("Area is "+ area);
+}
+
+  static int lengthinpt(){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter length: ");
+return sc.nextInt();
+}
+
+static int widthinpt(){
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter width: ");
+  return sc.nextInt();
+}}
