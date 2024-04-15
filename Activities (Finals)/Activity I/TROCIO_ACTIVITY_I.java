83310b5d71d65145d78ee054e7bd89d0db100920
Jushua Trocio <156936077+Jushua0909@users.noreply.github.com>
2024-04-15 16:41:15 +0800

    Update TROCIO_ACTIVITY_I.java

--- Activities (Finals)/Activity I/TROCIO_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/TROCIO_ACTIVITY_I.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
 public class TROCIO_ACTIVITY_I {
-   public static void main(String []args){
-    int length = getLength();
-    int width = getWidth();
-    int area = length * width;
-    System.out.println("Area = "+area);
-   }
-
-   public static int getLength(){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the length of the rectangle: ");
-    return sc.nextInt();
-   }
-
-   public static int getWidth(){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the width of the rectangle: ");
-    return sc.nextInt();
-   }
+    static int width(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the width of the rectangle:");
+        return sc.nextInt();
     }
+
+    static int length(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle:");
+        return sc.nextInt();
+    }
+
+    public static void main(String []args){
+    
+        int result = width() * length();
+        System.out.println("Area of the rectangle: " + result);
+
+    }
+
+}
