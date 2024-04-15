3a3b300f6b6471c2e7c60dbe2421c24735ef25a1
jemarjardin23 <157579137+jemarjardin23@users.noreply.github.com>
2024-04-15 16:40:44 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/JARDIN_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class JARDIN_ACTIVITY_I{
+public static void main(String[] args) {
+    int length = getLength();
+    int width = getWidth();
+    int area = length * width;
+    System.out.println("Area =" +area);
+}
+public static int getLength(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Length Of The Rectangle: ");
+    return sc.nextInt();
+}
+public static int getWidth(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Width Of The Rectangle: ");
+    return sc.nextInt();
+}
+}
+    
