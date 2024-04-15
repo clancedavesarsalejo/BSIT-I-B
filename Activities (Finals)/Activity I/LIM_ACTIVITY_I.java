871c05a949e5b8672ebd45cc5bac6eb31ba14663
JoshuaLim <157562860+JoshuaLim2024@users.noreply.github.com>
2024-04-15 17:23:07 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/LIM_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LIM_ACTIVITY_I {
+  static void width(int x){
+    x = 0;
+  }
+  static void length(int y){
+    y = 0;
+  }
+  public static void main(String []args){
+    int formula = x * y;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Input value for width: ");
+    int x = sc.nextInt();
+    System.out.print("Input value for length: ");
+    int y = sc.nextInt();
+
+    System.out.println(formula):
+    sc.close();
+  }
+}
