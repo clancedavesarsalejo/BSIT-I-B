92ff6a30a77e1a959d012d16978fc3d0a47608d2
BayatonFrankie <156895146+BayatonFrankie@users.noreply.github.com>
2024-04-15 17:24:00 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BAYATON_ACTIVITY_II.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BAYATON_ACTIVITY_II {
+    
+    static int getfeet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Feet: ");
+        return sc.nextInt();
+    }
+    public static void main(String []args){
+        int feet = getfeet();
+        int inches = feet * 12;
+        System.out.println(inches);
+      
+    }
+    
+}
+    
+    
+
+
+
+
+
+
+     
+
+
+        
+
+    
