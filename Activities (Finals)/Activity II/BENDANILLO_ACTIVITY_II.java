5c5eee243e55fe5f3dc04608c7f31e492d8f89ab
GlyzaEncarquez <157580265+GlyzaEncarquez@users.noreply.github.com>
2024-04-29 23:55:44 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/BENDANILLO_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BENDANILLO_ACTIVITY_II{
+    static int getfeet (){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter Feet");
+        return sc.nextInt();
+    }
+    public static void main(String []args){
+        int feet = getfeet();
+        int inches = feet * 12;
+        System.out.println(inches);  
+        
+          }
+
+}
