d74f6c6037bba5ae244e3c80db6988615e15fb45
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-05-06 16:12:55 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/ACAYLAR_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ACAYLAR_ACTIVITY_I {
+    public static void main(String[] args) {
+        int length = lengthinpt();
+        int width = widthinpt();
+        int area = length * width;
+        System.out.println("Area is " + area);
+    }
+
+    static int lengthinpt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length: ");
+        return sc.nextInt();
+    }
+
+    static int widthinpt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        return sc.nextInt();
+    }
+}
