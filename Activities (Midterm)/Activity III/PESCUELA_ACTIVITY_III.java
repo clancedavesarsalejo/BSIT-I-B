9f8c1d54cae65a9291b3d19d8e445b46963c248d
RayLeonardPescuela <157578744+RayLeonardPescuela@users.noreply.github.com>
2024-05-08 10:02:41 +0800

    Update PESCUELA_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/PESCUELA_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/PESCUELA_ACTIVITY_III.java
@@ -1,21 +1,26 @@
-   import java.util.Scanner;
-   public class PESCUELA_ACTIVITY_III{
-        public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            int [][] scores = new int[5][5];
-            for(int x = 0; x  < scores.length; x++){
-                for(int y = 0; y  < scores[x].length; y++){
-                    System.out.print("Enter the column "+(x+1)+ " of row " + (y+1) + ": ");
-                    scores[x][y] = sc.nextInt();
-   
-                }
-                for (int x = 0; x < scores.length; x++) {
-                    int sum = 0;
-                    for (int y = 0; y < scores[x].length; y++) {
-                        sum += scores[x][y];
-                    }
-                    double average = (double) sum / scores[x].length;
-                    System.out.println("Average score for row " + (x + 1) + ": " + average);
-                }
-            }
-        }
+import java.util.Scanner;
+public class PESCUELA_ACTIVITY_III{
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+
+		int rows = 5;
+		int columns = 5;
+		int[][] scores = new int[rows][columns];
+        
+		for (int x = 0; x < rows; x++) {
+		    for (int y = 0; y < columns; y++) {
+		    System.out.print("Enter score : ");
+		    scores[x][y] = sc.nextInt();
+		    }
+	    }
+		int sum = 0;
+		for (int x = 0; x < rows; x++) {
+		    for (int y = 0; y < columns; y++) {
+		    sum += scores[x][y];
+		    }
+	    }
+
+		double average = (double) sum / (rows * columns);
+		System.out.println("Average = " + average);
+    }
+}
