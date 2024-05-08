1333f90ebb2112fe7dc7f0bfc635a3cdf2f771b0
RayLeonardPescuela <157578744+RayLeonardPescuela@users.noreply.github.com>
2024-05-08 10:18:01 +0800

    Update PESCUELA_ACTIVITY_II.java

--- Activities (Finals)/Activity II/PESCUELA_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/PESCUELA_ACTIVITY_II.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class PESCUELA_ACTIVITY_II {
-    static void conversion(int feet, int inches){
-        int convert = feet * inches;
+    static void conversion(int feet){
+        int convert = feet * 12;
         System.out.println("Inches: " + convert);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of feet: ");
         int feet = sc.nextInt();
-        conversion(feet,12);
+        conversion(feet);
     }
 }
