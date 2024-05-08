db02b5d8e22eb76f007c54379dddc9b5b8d28140
yvonne pono <157579481+yv0nne1@users.noreply.github.com>
2024-05-08 22:10:09 +0800

    Update PONO_ACTIVITY_VI.java

--- Activities/Activity VI/PONO_ACTIVITY_VI.java
+++ Activities/Activity VI/PONO_ACTIVITY_VI.java
@@ -1,3 +1,18 @@
 import java.util.Scanner;
      public class PONO_ACTIVITY_VI {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter your timer: ");
+            int time = input.nextInt();
+
+            while(time>=0){
+                System.out.println(time);
+                time--;
+            }
+
+        }
+
+    }
         
