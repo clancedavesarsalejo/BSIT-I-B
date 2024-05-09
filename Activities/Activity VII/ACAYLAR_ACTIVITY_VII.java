c0358f6661cf83059ce131d6fa8692345f2a1a3b
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-05-09 13:03:10 +0800

    ACAYLAR_ACTIVITY_VII.java

--- Activities/Activity VII/ACAYLAR_ACTIVITY_VII.java
+++ Activities/Activity VII/ACAYLAR_ACTIVITY_VII.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-
-public class ACAYLAR_ACTIVITy_VII {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Factorial Number: ");
-        long number = sc.nextInt();
-        long fact = 1;
-        for (i = 1; i <= number; i++) {
-            fact *= i;
-        }
-        System.out.print(fact);
-
+public class ACAYLAR_ACTIVITY_VII {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your Number: ");
+    long number = sc.nextInt();
+    long fact = 1;
+    for(long x=1; x <= number;x++){
+        fact*=x;
     }
+    System.out.print("Factorial of "+number+" is "+fact);
+
+    
+}
 }
