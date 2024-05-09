bb66d49062682b0bfe097e3fbe79a2549f6ad25a
Joseph M. Generale <156919590+josephgenerale@users.noreply.github.com>
2024-05-09 16:11:51 +0800

    Update and rename Generale_Activity_III.java to GENERALE_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/GENERALE_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/GENERALE_ACTIVITY_III.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Generale_Activity_III {
+public class GENERALE_ACTIVITY_III {
     public static void main (String []args){
         
         Scanner sc = new Scanner(System.in);
