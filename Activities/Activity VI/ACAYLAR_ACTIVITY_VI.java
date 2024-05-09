7d6f8023b75d57adcdb97512c40279b6f52b01ef
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-05-09 13:00:38 +0800

    ACAYLAR_ACTIVITY_VI.java

--- Activities/Activity VI/ACAYLAR_ACTIVITY_VI.java
+++ Activities/Activity VI/ACAYLAR_ACTIVITY_VI.java
@@ -4,14 +4,31 @@ public class ACAYLAR_ACTIVITy_VI {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sec = 5;
-        System.out.print("Enter the Countdown: ");
-        int time = sc.nextInt();
 
-        while (sec >= 0) {
-            System.out.println(sec);
-            sec--;
+        while (true) {
+            System.out.print("Enter the Countdown: ");
+            int time = sc.nextInt();
+
+            int sec = time; 
+
+            while (sec >= 0) {
+                System.out.println(sec);
+                sec--;
+            }
+
+            System.out.println("Countdown completed.");
+
+            
+            System.out.print("Do you want to start a new countdown? (yes/no): ");
+            String answer = sc.next();
+
+            if (!answer.equalsIgnoreCase("yes")) {
+                System.out.println("Exiting the program.");
+                break; 
+            }
         }
+
+        sc.close();
     }
 
 }
