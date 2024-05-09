89d907a8c111c169bd85a2d109672d7f229ef470
Joseph M. Generale <156919590+josephgenerale@users.noreply.github.com>
2024-05-09 16:06:58 +0800

    Update and rename Generale_Activity_II.java to GENERALE_ACTIVITY_II.java

--- Activities/Activity II/GENERALE_ACTIVITY_II.java
+++ Activities/Activity II/GENERALE_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Generale_Activity_II {
+public class GENERALE_ACTIVITY_II {
     public static void main (String []args) {
             Scanner sc = new Scanner(System.in);
             
