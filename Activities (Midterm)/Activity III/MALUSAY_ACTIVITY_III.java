793d555a459e02e75e2bfb457b45ae4bffb1c3ed
juliefel <157578767+juliefel12@users.noreply.github.com>
2024-05-09 13:45:57 +0800

    Update MALUSAY_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/MALUSAY_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/MALUSAY_ACTIVITY_III.java
@@ -1,43 +1,37 @@
 import java.util.Scanner;
-public class MALUSAY_ACTIVITY_III {
- public static void main (String []args){
-    Scanner sr = new Scanner (System.in);
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public class MALUSAY_ACTIVITY_III {
+        public static void main(String[] args) {
+            Scanner sr = new Scanner (System.in);
 
-        int[][] Score = {
-            {1, 2, 3, 4, 5},
-            {6, 7, 8, 9, 10},
-            {11, 12, 13, 14, 15},
-            {16, 17, 18, 19, 20},
-            {21, 22, 23, 24, 25}
-        };
-        
-        int sum = 0; 
+    final int rows = 5;
+    final int cols = 5;
 
-        for (int i = 0; i < 25; i++) {
-            System.out.print("Input Score" + ": ");
-            int score = sc.nextInt();
-            
-            sum += score; 
-            
-            
-            for (int x = 0; x < Score.length; x++) {
-                for (int y = 0; y < Score[x].length; y++) {
-                    if (Score[x][y] == score) {
-                        System.out.println("Score at position (" + x + "," + y + ") is " + score);
-                    }
-                }
-            }
+    int[][] scores = new int[rows][cols];
+    Scanner scanner = new Scanner(System.in);
+
+
+    System.out.println("Enter scores for 5 students:");
+    for (int i = 0; i < rows; i++) {
+        System.out.println("Enter scores for student " + (i + 1) + ": ");
+        for (int j = 0; j < cols; j++) {
+            scores[i][j] = scanner.nextInt();
         }
-        
-        System.out.println("Sum of all scores: " + sum); 
-        
-        double average = (double) sum / 25; 
-        System.out.println("Average of all scores: " + average); 
     }
-}
 
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum += scores[i][j];
+            count++;
+        }
+    }
+    double average = (double) sum / count;
+
+    System.out.println("Average of all scores: " + average);
+    scanner.close();
+}
+}
 
 
 
