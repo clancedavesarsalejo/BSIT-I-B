4ed268c7ed37cfcc01726606c2fdce270a9cb41d
JoshuaLim <157562860+JoshuaLim2024@users.noreply.github.com>
2024-05-09 12:17:20 +0800

    Update LIM_ACTIVITY_II.java

--- Activities/Activity II/LIM_ACTIVITY_II.java
+++ Activities/Activity II/LIM_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LIM_Activity_II {
+public class LIM_ACTIVITY_II {
     public static void main (String []args) {
             Scanner sc = new Scanner(System.in);
             
