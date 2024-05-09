395ced236812a746777d34d759ee909a8a9d97ba
CareyAcaylar <157579052+CareyAcaylar@users.noreply.github.com>
2024-05-09 12:53:17 +0800

    ACAYLAR_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/ACAYLAR_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/ACAYLAR_ACTIVITY_III.java
@@ -1,35 +1,32 @@
+import java.util.Scanner;
+
 public class ACAYLAR_ACTIVITY_III {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] Score = {
-                { 1, 2, 3, 4, 5 },
-                { 6, 7, 8, 9, 10 },
-                { 11, 12, 13, 14, 15 },
-                { 16, 17, 18, 19, 20 },
-                { 21, 22, 23, 24, 25 }
+        int[][] scores = {
+            { 1, 2, 3, 4, 5 },
+            { 6, 7, 8, 9, 10 },
+            { 11, 12, 13, 14, 15 },
+            { 16, 17, 18, 19, 20 },
+            { 21, 22, 23, 24, 25 }
         };
 
         int sum = 0;
 
-        for (int i = 0; i < 25; i++) {
-            System.out.print("Input Score" + ": ");
-            int score = sc.nextInt();
-
-            sum += score;
-
-            for (int x = 0; x < Score.length; x++) {
-                for (int y = 0; y < Score[x].length; y++) {
-                    if (Score[x][y] == score) {
-                        System.out.println("Score at position (" + x + "," + y + ") is " + score);
-                    }
-                }
+       
+        for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j < scores[i].length; j++) {
+                System.out.println("Score at position (" + i + "," + j + ") is " + scores[i][j]);
+                sum += scores[i][j];
             }
         }
 
         System.out.println("Sum of all scores: " + sum);
 
-        double average = (double) sum / 25;
+        double average = (double) sum / (scores.length * scores[0].length);
         System.out.println("Average of all scores: " + average);
+
+        sc.close(); 
     }
 }
