343c39209bee5f35c7c0b685fe80a9ad6a5883cc
juliefel <157578767+juliefel12@users.noreply.github.com>
2024-05-09 13:28:46 +0800

    Update MALUSAY_ACTIVITY_V.java

--- Activities/Activity V/MALUSAY_ACTIVITY_V.java
+++ Activities/Activity V/MALUSAY_ACTIVITY_V.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
-    public class MALUSAY_ACTIVITY_V{
-        
-        public static void main(String []args){
-            Scanner Age = new Scanner(System.in);
+public class MALUSAY_ACTIVITY_V{
+    public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
 
-            System.out.print( "Enter Age: ");
-            int age = Age.nextInt();
+	System.out.print("Enter your age : ");
+	int age=sc.nextInt();
 
-            if (age ==0 && age <=11){
-                System.out.println("Child");
-            } else if (age>= 12 && age <= 17)
-            {
-                System.out.println("Teen");
-            } else if (age >= 18 && age>= 64)
-            {
-                System.out.println("Adult");
-             }   
-            
-        
-            
+	if (age >= 0 && age <= 11){
+        System.out.println("Child");
+        }
+    else if (age >= 12 && age <= 17){
+        System.out.println("Teen");
+	    }
+    else if (age >= 18 && age <= 64){
+	System.out.println("Adult");
+	}
+    else{
+	    System.out.println("Reached Age Limit");
         }
     }
+}
